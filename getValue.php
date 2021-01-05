0da9a9888b5399557f209013a504c6dfa7997e27
Carl Reed <carlreedw@gmail.com>
2021-01-05 15:06:05 -0600

    update getValue to return instance data most of the time, for msot field types

--- getValue.php
+++ getValue.php
@@ -3,13 +3,24 @@
 	
 	use ExternalModules\AbstractExternalModule;
 	use ExternalModules\ExternalModules;
-
+	
+	$module->llog("\$_GET: " . print_r($_GET, true) . "\n");
+	$module->llog("\$_POST: " . print_r($_POST, true) . "\n");
+	
 	require_once APP_PATH_DOCROOT.'Classes/LogicTester.php';
 
 	if($_POST['otherpid'] != $_POST['thispid']) {
 		\REDCap::allowProjects(array($_POST['otherpid'], $_POST['thispid']));
 	}
-
+	$sourceProject = new \Project(intval($_POST['otherpid']));
+	try {
+		$choices_obj = json_decode($_POST['choices'], true);
+	} catch(\Exception $e) {
+		$choices_obj = new \stdClass();;
+	}
+	// $active_forms = $module->getProjectSetting('active-forms');
+	$instance_i = intval($_GET['instance']);
+	
 	$thisjson = \REDCap::getData($_POST['thispid'], 'json', array($_POST['thisrecord']), array($_POST['thismatch'])); 
 	$thismatch = trim($_POST['thismatch']);
 	$thismatch = preg_replace("/^[\'\"]/", "", $thismatch);
@@ -81,20 +92,31 @@
 	} else {
 		$logicItems = array($fieldName => $logic);
 	}
-
-
-	$found = false;
-	foreach ($data as $record => $recData) {
-		$Proj = new \Project($_POST['otherpid']);
+	
+	// this function returns
+	// return field value or NULL
+	function exitInstanceValue($instance) {
+		global $sourceProject;
+		global $logicItems;
+		global $choices_obj;
+		global $module;
+		
+		// $module->llog("exitInstanceValue instance argument: " . print_r($instance, true));
+		$module->llog("exitInstanceValue logicItems: " . print_r($logicItems, true));
+		
+		$eid = $sourceProject->firstEventId;
 		foreach ($logicItems as $field => $logicItem) {
+			$module->llog("field $field, logicItem $logicItem");
 			if (\LogicTester::isValid($logicItem)) {
-				$result = \LogicTester::apply($logicItem, $recData, $Proj, true);
+				$result = \LogicTester::apply($logicItem, $instance, $sourceProject, true);
+				
+				$inst_field = $instance[$eid][substr($logicItem, 1, -1)];
+				
 				if ($result || $result === "0" || $result === 0) {
-					$choices = json_decode($_POST['choices'], true);
-					if (isset($choices[$field])) {
-						if (isset($choices[$field][$result])) {
+					if (isset($choices_obj[$field])) {
+						if (isset($choices_obj[$field][$result])) {
 							if ($_POST['getlabel'] != 0) {
-								$returnVal = $choices[$field][$result];
+								$returnVal = $choices_obj[$field][$result];
 							} else {
 								$returnVal = $result;
 							}
@@ -104,19 +126,47 @@
 					} else {
 						$returnVal = $result;
 					}
-					echo $returnVal;
-					$found = true;
-					break;
-				} else if(!empty($recData[$Proj->firstEventId][substr($logicItem, 1, -1)]) && is_array($recData[$Proj->firstEventId][substr($logicItem, 1, -1)])) {
+					$module->llog('exit a: ' . print_r($returnVal, true));
+					exit($returnVal);
+				// } else if(!empty($instance[$eid][substr($logicItem, 1, -1)]) && is_array($instance[$eid][substr($logicItem, 1, -1)]) && !empty(reset($instance[$eid][substr($logicItem, 1, -1)]))) {
+				} else if(!empty($inst_field) && is_array($inst_field) && (reset($inst_field) == '0' or reset($inst_field) == '1')) {
 					header('Content-Type: application/json');
-					echo json_encode($recData[$Proj->firstEventId][substr($logicItem, 1, -1)]);
-					$found = true;
-					break;
+					$module->llog('exit b: ' . print_r(json_encode($instance[$eid][substr($logicItem, 1, -1)]), true));
+					$module->llog("exitInstanceValue instance argument: " . print_r($instance, true));
+					exit(json_encode($instance[$eid][substr($logicItem, 1, -1)]));
 				}
-			}
-			if ($found) {
-				break;
 			}
 		}
 	}
+	
+	foreach ($data as $record => $recData) {
+		// $module->llog("calling exitInstanceValue on recData: " . print_r($recData, true));
+		exitInstanceValue($recData);
+		
+		// if we didn't exit a value, didn't find one, check repeating instances
+		if ($instance_i <= 0) {
+			$module->llog('exit c: ""');
+			exit("");
+		}
+		$module->llog('depth level a');
+		foreach($recData['repeat_instances'] as $eid => $instruments) {
+			$module->llog('depth level b');
+			foreach ($instruments as $instrument_name => $instances) {
+				$module->llog('depth level c');
+				// ensure instrument is in active_forms setting
+				$instance = $instances[$instance_i];
+				// $module->llog("instances[instance_i: $instance_i]: " . print_r($instance, true));
+				if (!empty($instance)) {
+					$module->llog('depth level d');
+					// shim instance so it looks like regular, non-repeating instrument data
+					// $module->llog("calling exitInstanceValue on: " . print_r([$eid => $instance], true));
+					exitInstanceValue([$eid => $instance]);
+				}
+			}
+		}
+	}
+	
+	// send empty value response
+	$module->llog('exit d: ""');
+	exit("");
 ?>
